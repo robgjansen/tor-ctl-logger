48dc98bdbd7c35b5ebe5ff41514fa1128576f1dc
Rob Jansen <jansen@cs.umn.edu>
2021-04-08 11:22:58 -0400

    Support python 3 and control sockets

--- tor-ctl-logger.py
+++ tor-ctl-logger.py
@@ -21,7 +21,12 @@ def main():
     parser.add_argument('-p', '--port',
         help="""the Tor control port number N""",
         action="store", type=int, metavar="N",
-        dest="ctlport", required=True)
+        dest="ctlport", required=False, default=None)
+
+    parser.add_argument('-s', '--socket',
+        help="""Path to the Tor control socket""",
+        action="store", type=str, metavar="PATH",
+        dest="ctlsocket", required=False, default=None)
 
     parser.add_argument('-l', '--logpath',
         help="""a STRING path to log Tor controller output""",
@@ -45,6 +50,10 @@ def main():
     args = parser.parse_args()
     args.logpath = os.path.abspath(os.path.expanduser(args.logpath))
 
+    if args.ctlport == None and args.ctlsocket == None:
+        print("Please specify either a control port or control socket. See 'tor-ctl-logger --help' for more info.")
+        return
+
     try:
         run(args)
     except KeyboardInterrupt:
@@ -52,50 +61,58 @@ def main():
 
 def run(args):
     with open(args.logpath, 'a') as logfile:
-        startup_msg = "started tor-ctl-logger on port {0}, logging events to {1}\n".format(args.ctlport, args.logpath)
+        ctl_loc = args.ctlsocket if args.ctlsocket != None else args.ctlport
+        startup_msg = "started tor-ctl-logger on {0}, logging events to {1}\n".format(ctl_loc, args.logpath)
         __log(logfile, startup_msg)
         __log(sys.stderr, startup_msg)
 
-        with stem.control.Controller.from_port(port = args.ctlport) as torctl:
-            torctl.authenticate()
+        if args.ctlsocket != None:
+            with stem.control.Controller.from_socket_file(path = args.ctlsocket) as torctl:
+                __run_helper(args, logfile, torctl)
+        elif args.ctlport != None:
+            with stem.control.Controller.from_port(port = args.ctlport) as torctl:
+                __run_helper(args, logfile, torctl)
 
-            # register for async events!
-            # some events are only supported in newer versions of tor, so ignore errors from older tors
-            event_handler = partial(__handle_tor_event, logfile, )
-            events_added = []
+def __run_helper(args, logfile, torctl):
+    torctl.authenticate()
 
-            for e in args.events:
-                assert e in EventType
-                # add all configured events that our stem version supports
-                # warn and ignore those that our stem supports but our Tor does not
-                try:
-                    torctl.add_event_listener(event_handler, EventType[e])
-                    events_added.append(e)
-                except:
-                    warn_msg = "[WARNING] event '{}' is recognized by stem but not by tor\n".format(e)
-                    __log(logfile, warn_msg)
-                    pass
+    # register for async events!
+    # some events are only supported in newer versions of tor, so ignore errors from older tors
+    event_handler = partial(__handle_tor_event, logfile, )
+    events_added = []
 
-            for e in args.custom_events:
-                    try:
-                        torctl.add_event_listener(event_handler, e)
-                        events_added.append(e)
-                    except:
-                        err_msg = "[ERROR] event '{}' is not recognized by tor\n".format(e)
-                        __log(logfile, err_msg)
-                        __log(sys.stderr, err_msg)
-                        return
-            __log(logfile, "registered for the following events: {0}\n".format(' '.join(events_added)))
+    for e in args.events:
+        assert e in EventType
+        # add all configured events that our stem version supports
+        # warn and ignore those that our stem supports but our Tor does not
+        try:
+            torctl.add_event_listener(event_handler, EventType[e])
+            events_added.append(e)
+        except:
+            warn_msg = "[WARNING] event '{}' is recognized by stem but not by tor\n".format(e)
+            __log(logfile, warn_msg)
+            pass
 
-            # let stem run its threads and log all of the events, until user interrupts
+    for e in args.custom_events:
             try:
-                while True:
-                    with open(args.logpath, 'rb') as sizef:
-                        msg = "heartbeat: logged {0} bytes to {1}, press CTRL-C to quit\n".format(os.fstat(sizef.fileno()).st_size, args.logpath)
-                        __log(sys.stderr, msg)
-                    time.sleep(60)
-            except KeyboardInterrupt:
-                pass  # the user hit ctrl+c
+                torctl.add_event_listener(event_handler, e)
+                events_added.append(e)
+            except:
+                err_msg = "[ERROR] event '{}' is not recognized by tor\n".format(e)
+                __log(logfile, err_msg)
+                __log(sys.stderr, err_msg)
+                return
+    __log(logfile, "registered for the following events: {0}\n".format(' '.join(events_added)))
+
+    # let stem run its threads and log all of the events, until user interrupts
+    try:
+        while True:
+            with open(args.logpath, 'rb') as sizef:
+                msg = "heartbeat: logged {0} bytes to {1}, press CTRL-C to quit\n".format(os.fstat(sizef.fileno()).st_size, args.logpath)
+                __log(sys.stderr, msg)
+            time.sleep(60)
+    except KeyboardInterrupt:
+        pass  # the user hit ctrl+c
 
 def __handle_tor_event(logfile, event):
     __log(logfile, event.raw_content())
@@ -105,6 +122,6 @@ def __log(logfile, msg):
     utcnow = datetime.datetime.utcnow()
     epoch = datetime.datetime(1970, 1, 1)
     unix_ts = (utcnow - epoch).total_seconds()
-    print >>logfile, "{0} {1:.02f} {2}".format(now.strftime("%Y-%m-%d %H:%M:%S"), unix_ts, msg),
+    print("{0} {1:.02f} {2}".format(now.strftime("%Y-%m-%d %H:%M:%S"), unix_ts, msg).strip(), file=logfile)
 
 if __name__ == '__main__': sys.exit(main())
